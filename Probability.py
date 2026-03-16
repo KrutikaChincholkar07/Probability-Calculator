d841d303908178d0b4cd4df2b46de63b25a5dfe7
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2026-03-16 23:44:57 +0530

    Create Probability.py
    Example: Probability of getting a 4 when rolling a dice
    Total outcomes = 6
    Favorable outcomes = 1
    Algorithm
    Start the program
    Input favorable outcomes from the user
    Input total outcomes from the user
    Calculate probability using the formula
    P = favorable / total
    Display the probability
    End the program

---
+++ Probability.py
@@ -0,0 +1,6 @@
+favorable = float(input("Enter number of favorable outcomes: "))
+total = float(input("Enter total number of outcomes: "))
+
+probability = favorable / total
+
+print("Probability =", probability)
